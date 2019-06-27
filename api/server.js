633a0061ea6a7d37b58487e0a631f5392ebd0679
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-27 14:40:06 -0400

    Commenting out unfiinished routes.

--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const booksRouter = require("../routers/booksRouter.js");
-const reviewsRouter = require("../routers/reviewsRouter.js");
+// const booksRouter = require("../routers/booksRouter.js");
+// const reviewsRouter = require("../routers/reviewsRouter.js");
 const authRouter = require("../routers/authRouter.js");
 const usersRouter = require("../routers/usersRouter.js");
 const authorization = require("../routers/authorization.js");
@@ -13,8 +13,8 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use("/api/books", authorization, booksRouter);
-server.use("/api/reviews", authorization, reviewsRouter);
+// server.use("/api/books", authorization, booksRouter);
+// server.use("/api/reviews", authorization, reviewsRouter);
 server.use("/api/auth", authRouter);
 server.use("/api/users", authorization, usersRouter);
 
